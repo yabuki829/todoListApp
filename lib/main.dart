f980931b88756630b2eab4e22c6d4cc3a9ef93c8
yabuki829 <syodai829@icloud.com>
2024-10-06 20:51:02 +0900

    darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/notifier/theme_notifier.dart';
 import 'package:todoapp/routers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:todoapp/utils/share_preference.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await SharedPreferencesInstance.initialize();
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: router,
@@ -20,6 +24,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      themeMode: ref.watch(themeModeNotifierProvider),
+      darkTheme: ThemeData.dark(),
     );
   }
 }
