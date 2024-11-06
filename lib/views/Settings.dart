30dec526f2063e19d006b1858ff1e5d5651b8d00
yabuki829 <syodai829@icloud.com>
2024-11-07 07:43:21 +0900

    feat: タブの入れ替え機能

--- lib/views/Settings.dart
+++ lib/views/Settings.dart
@@ -1,55 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:todoapp/notifier/theme_notifier.dart';
+import 'package:todoapp/views/change_tab_order.dart';
 
-class SettingsView extends StatefulWidget {
+class SettingsView extends ConsumerWidget {
   const SettingsView({super.key});
 
   @override
-  State<SettingsView> createState() => _SettingsViewState();
-}
-
-class _SettingsViewState extends State<SettingsView> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final toggle = ref.read(themeModeNotifierProvider.notifier).toggle;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: const Text(
-          "設定",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text(
+            "設定",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
           ),
         ),
-      ),
-      body: Consumer(
-        builder: (context, ref, _) {
-          final toggle = ref.read(themeModeNotifierProvider.notifier).toggle;
-          switch (ref.watch(themeModeNotifierProvider)) {
-            case ThemeMode.light:
-              return ListTile(
-                leading: const Icon(Icons.light_mode_rounded),
-                title: const Text('ライトモード'),
-                onTap: toggle,
-              );
-            case ThemeMode.dark:
-              return ListTile(
-                leading: const Icon(Icons.dark_mode_rounded),
-                title: const Text('ダークモード'),
-                onTap: toggle,
-              );
-            case ThemeMode.system:
-              return ListTile(
-                leading: const Icon(Icons.smartphone_rounded),
-                title: const Text('システムの設定に合わせる'),
-                onTap: toggle,
-              );
-            default:
-              return const SizedBox.shrink();
-          }
-        },
-      ),
-    );
+        body: Column(
+          children: [
+            _buildThemeModeTile(ref, toggle),
+            ListTile(
+              leading: const Icon(Icons.change_circle),
+              title: const Text('タブの入れ替え'),
+              onTap: () {
+                showModalBottomSheet(
+                    useRootNavigator: true,
+                    context: context,
+                    builder: (context) => const ChangeTabOrderView());
+              },
+            ),
+          ],
+        ));
+  }
+
+  Widget _buildThemeModeTile(WidgetRef ref, VoidCallback toggle) {
+    switch (ref.watch(themeModeNotifierProvider)) {
+      case ThemeMode.light:
+        return ListTile(
+          leading: const Icon(Icons.light_mode_rounded),
+          title: const Text('ライトモード'),
+          onTap: toggle,
+        );
+      case ThemeMode.dark:
+        return ListTile(
+          leading: const Icon(Icons.dark_mode_rounded),
+          title: const Text('ダークモード'),
+          onTap: toggle,
+        );
+      case ThemeMode.system:
+        return ListTile(
+          leading: const Icon(Icons.smartphone_rounded),
+          title: const Text('システムの設定に合わせる'),
+          onTap: toggle,
+        );
+      default:
+        return const SizedBox.shrink();
+    }
   }
 }
